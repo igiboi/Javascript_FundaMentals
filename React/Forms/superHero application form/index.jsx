b56660243ffe3625cd1845d96499e0d16fe94c13
Luigi Matias <igiboi26@gmail.com>
2026-08-24 18:28:12 -0400

    draft for jsx file

---
+++ React/Forms/superHero application form/index.jsx
@@ -0,0 +1,39 @@
+const { useState } = React;
+
+export const SuperheroForm = () => {
+  const [heroName, setHeroName] = useState("");
+  const [realName, setRealName] = useState("");
+  const [powerSource, setPowerSource] = useState("");
+  const [powers, setPowers] = useState([]);
+
+  return (
+    <div className='form-wrap'>
+      <h2>Superhero Application Form</h2>
+      <p>Please complete all fields</p>
+      <form>
+        <div className='section'>
+          <label>
+            Hero Name
+            <input
+              type='text'
+              value={heroName}
+              onChange={(e) => setHeroName(e.target.value)}
+            />
+          </label>
+          <label>
+            Real Name
+            <input
+              type='password'
+              value={realName}
+              onChange={(e) => setRealName(e.target.value)}
+            />
+          </label>
+        </div>
+        <label className='section column'>
+          How did you get your powers?
+          <select></select>
+        </label>
+      </form>
+    </div>
+  );
+};
